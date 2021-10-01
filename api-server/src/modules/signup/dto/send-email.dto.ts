d6a628ac4c85178aa85feebc163b7d6663c3a822
jiyoung.lim <swimjiy@krafton.com>
2021-10-02 01:40:00 +0900

    feat: add signup swagger

--- api-server/src/modules/signup/dto/send-email.dto.ts
+++ api-server/src/modules/signup/dto/send-email.dto.ts
@@ -1,6 +1,19 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail } from 'class-validator';
 
 export class SendEmailDto {
+  @ApiProperty({ example: 'mashup.anonymous@gmail.com' })
   @IsEmail()
   email: string;
+}
+
+export class SendEmailResponseDto {
+  @ApiProperty()
+  isSend?: boolean;
+
+  @ApiPropertyOptional({
+    default: false,
+    description: 'Optional. 유저가 존재하는 경우 true',
+  })
+  isUserExist?: boolean;
 }
--- api-server/src/modules/signup/dto/verify-code.dto.ts
+++ api-server/src/modules/signup/dto/verify-code.dto.ts
@@ -1,7 +1,28 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail } from 'class-validator';
 
 export class VerifyCodeDto {
   @IsEmail()
+  @ApiProperty({ example: 'mashup.anonymous@gmail.com' })
   email: string;
+
+  @ApiProperty({ example: '1a2b3c' })
   code: string;
+}
+
+export class VerifyCodeResponseDto {
+  @IsEmail()
+  @ApiProperty({ example: 'mashup.anonymous@gmail.com' })
+  email: string;
+
+  @ApiPropertyOptional({
+    default: false,
+    description: 'Optional. 만료된 코드면 true',
+  })
+  isCodeExpired?: boolean;
+
+  @ApiPropertyOptional({
+    description: 'Optional. 코드 일치 여부',
+  })
+  isVerify?: boolean;
 }
