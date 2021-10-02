7a8a81b5811c33e5eaff8fdd6304c5e89de49d98
Karl Saehun Chung <nycom13@gmail.com>
2021-10-03 01:07:16 +0900

    feat(api): add slack event module (#4)

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -3,20 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfig from './core/config/database.config';
+import slackConfig from './core/config/slack.config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SignupModule } from './modules/signup/signup.module';
+import { SlackModule } from './modules/slack/slack.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [databaseConfig],
+      load: [databaseConfig, slackConfig],
     }),
+    EventEmitterModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => config.get('database'),
       inject: [ConfigService],
     }),
     SignupModule,
+    SlackModule,
   ],
   controllers: [AppController],
   providers: [AppService],
