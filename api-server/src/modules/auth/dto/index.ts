6ace73485bc9467d14620886ac4a870dddf60690
jiyoung.lim <swimjiy@gmail.com>
2021-10-27 22:21:26 +0900

    feat(auth): refactor dtos

---
+++ api-server/src/modules/auth/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './login.dto';
+export * from './send-email.dto';
+export * from './sign-up.dto';
+export * from './update-password.dto';
+export * from './validate-nickname.dto';
+export * from './verify-code.dto';
--- api-server/src/modules/auth/dto/update-password.dto.ts
+++ api-server/src/modules/auth/dto/update-password.dto.ts
@@ -1,11 +1,14 @@
+import { IsString, IsBoolean } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdatePasswordDto {
-  @ApiProperty({ example: 'qlalfqjsgh486' })
+  @IsString()
+  @ApiProperty()
   newPassword: string;
 }
 
 export class UpdatePasswordResponseDto {
-  @ApiProperty({ description: '변경 완료 되었으면 true', })
+  @IsBoolean()
+  @ApiProperty()
   isOk: boolean;
 }
