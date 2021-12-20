a3c778e9e71bcc5b41d6931f6c708adbb3ad8292
jiyoung.lim <swimjiy@gmail.com>
2021-12-21 05:03:18 +0900

    feat(api): filter search results that invalid for reporting

--- api-server/src/modules/search/search.controller.ts
+++ api-server/src/modules/search/search.controller.ts
@@ -4,9 +4,13 @@ import {
   Get,
   ParseIntPipe,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { Pagination } from 'nestjs-typeorm-paginate';
+import { AuthUser } from 'src/core/decorators/auth-user.decorator';
 import { Hole, Review } from 'src/entities/review.entity';
+import { JwtUser } from 'src/entities/user.entity';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { SearchService } from './search.service';
 import { docs } from './search.docs';
 import { ApiTags } from '@nestjs/swagger';
@@ -18,14 +22,17 @@ export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
   @Get('review')
+  @UseGuards(JwtAuthGuard)
   @docs.searchReview('리뷰 피드 요청 API')
   async searchReview(
+    @AuthUser() user: JwtUser,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
     @Query('userId') userId?: string,
     @Query('hole') hole?: Hole,
   ): Promise<Pagination<Review>> {
     return await this.searchService.searchReview(
+      user,
       { page, limit },
       { userId, hole },
     );
